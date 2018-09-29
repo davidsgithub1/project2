aeacb4b317a3b05ab3ef030a20cd6ef8e1f37667
velimirT <velimir.tchatchevsky@gmail.com>
2018-09-29 15:18:00 -0400

    fix server warning

--- server/server.js
+++ server/server.js
@@ -2,14 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 // const connection = require('../config/connection');
-const publicPath = path.join(__dirname, '..', 'build');
+const publicPath = path.join(__dirname, '../', 'build');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   console.log('test');
-  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {

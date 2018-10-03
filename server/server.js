02864f7bd4cf7cb58125d84e12526b95ab843a89
David <lee.david87@gmail.com>
2018-10-03 12:30:48 -0400

    server side rendering

--- server/server.js
+++ server/server.js
@@ -6,12 +6,31 @@ const routsBooks = require('./routsBooks');
 const publicPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 
+const React = require("react");
+const { renderToString } = require("react-dom/server");
+const App = require("../src/App");
+
 app.use(express.static(publicPath));
 
 routsBooks(app);
 
 app.get('/test', (req, res) => {
   console.log('test');
+});
+
+app.get("*", (req, res) => {
+  res.send(`
+      <!DOCTYPE html>
+      <head>
+        <title>Universal React</title>
+        <link rel="stylesheet" href="/css/main.css">
+        <script src="/bundle.js" defer></script>
+      </head>
+      <body>
+        <div id="root">${renderToString(<App />)}</div>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
-import Search from './containers/books/Search';
+import Books from './containers/books/Books';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      	<Search />
+      	<Books />
       </div>
     );
   }
